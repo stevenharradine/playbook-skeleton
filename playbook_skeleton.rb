30cb9424010fcc3fedd7ac66387deedeec254914
Aaron Pederson <aaron.pederson@gmail.com>
2015-07-09 16:08:16 -0700

    Lost part of repo; re-added infrastructure.yml creation.

--- playbook_skeleton.rb
+++ playbook_skeleton.rb
@@ -8,6 +8,7 @@ system "mkdir -p playbook-#{project_name}/hosts"
 system "touch playbook-#{project_name}/vault.yml"
 system "touch playbook-#{project_name}/dependencies.yml"
 system "touch playbook-#{project_name}/playbook.yml"
+system "touch playbook-#{project_name}/infrastructure.yml"
 
 environments.each { |environment| system "touch playbook-#{project_name}/hosts/#{environment}" }
 
